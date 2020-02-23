bdfee4c9cbc2a582be038cefe299f435100decf7
D-side <me@dside.ru>
2020-02-23 07:40:38 +0300

    Enable TLS in production

--- src/cljs/queueue/components/websocket.cljs
+++ src/cljs/queueue/components/websocket.cljs
@@ -1,13 +1,16 @@
 (ns queueue.components.websocket
   (:require [com.stuartsierra.component :as component]
-            [re-frame.core :as re-frame]))
+            [re-frame.core :as re-frame]
+            [queueue.config :refer [debug?]]))
 
 ; Horrible coupling, dispatching needs to happen in the app, not here
 ; Possibly use fx/cofx
 (defrecord WebSocket [ws]
   component/Lifecycle
   (start [component]
-    (let [ws (js/WebSocket. (str "ws://" (.-host (.-location js/window)) "/events"))]
+    (let [protocol (if debug? "ws" "wss") ; TLS is required on production deployment
+          ws-url (str protocol "://" (.-host (.-location js/window)) "/events")
+          ws (js/WebSocket. ws-url)]
       (.addEventListener ws "message" #(re-frame/dispatch [:ws-message %]))
       (assoc component :ws ws)))
   (stop [component]
